bd93d2c9353a11c20963b234c4e69615fcb4503e
Lucas <unmecpasfort@gmail.com>
2023-09-06 13:10:41 +0200

    PrintComb2 5

--- printcomb2.go
+++ printcomb2.go
@@ -8,14 +8,17 @@ func PrintComb2() {
 			for k := '0'; k <= '9'; k++ {
 				for l := '0'; l <= '9'; l++ {
 					if i != j {
-						z01.PrintRune(rune(i))
-						z01.PrintRune(rune(j))
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(k))
-						z01.PrintRune(rune(l))
-						if i != '9' || j != '9' || k != '9' || l != '9' {
-							z01.PrintRune(rune(','))
-							z01.PrintRune(rune(' '))
+						if (i != j || i != k) && (i != k || i != l) && (i != j || i != l) && (j != k || j != l) {
+							// XXX- 				X-XX                   	XX-X					-XXX
+							z01.PrintRune(rune(i))
+							z01.PrintRune(rune(j))
+							z01.PrintRune(' ')
+							z01.PrintRune(rune(k))
+							z01.PrintRune(rune(l))
+							if i != '9' || j != '9' || k != '9' || l != '9' {
+								z01.PrintRune(rune(','))
+								z01.PrintRune(rune(' '))
+							}
 						}
 					}
 				}
