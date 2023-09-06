d79b7d12846da3b3eee909472f4d611bfaa39755
Lucas <unmecpasfort@gmail.com>
2023-09-06 11:42:14 +0200

    PrintComb

---
+++ printcomb.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
+				if i != j && i != k && j != k {
+					z01.PrintRune(rune(i))
+					z01.PrintRune(rune(j))
+					z01.PrintRune(rune(k))
+					z01.PrintRune(rune(','))
+					z01.PrintRune(rune(' '))
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	PrintComb()
+}
