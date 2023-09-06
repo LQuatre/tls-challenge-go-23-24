f62df6b4e708b9ef3cf2ee36431ec49aa5da8269
Lucas <unmecpasfort@gmail.com>
2023-09-06 10:32:47 +0200

    Print Alphabet 3

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -6,13 +6,4 @@ func main() {
 	for letter := 97; letter < 123; letter++ {
 		z01.PrintRune(rune(letter))
 	}
-
-	invalidRune := rune(-1)
-	err := z01.PrintRune(invalidRune)
-	if err == nil {
-		panic("z01.PrintRune should fail with an invalid rune")
-	}
-	// Output:
-	// 01
-	// ♥
 }
