13086699bedd6b111e1fc0d594e5c0916e44175b
Lucas <unmecpasfort@gmail.com>
2023-09-06 18:25:22 +0200

    Printnbr 4

--- printnbr.go
+++ printnbr.go
@@ -4,41 +4,22 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func Itoa(n int) string {
-	var digits []int
-
-	if n == 0 {
+func IntToString(n int) string {
+	if n < 0 { // si n est négatif, on ajoute le signe moins au début
+		return "-" + IntToString(-n)
+	}
+	if n == 0 { // si n est zéro, on retourne "0"
 		return "0"
 	}
-
-	for n != 0 {
-		digits = append(digits, n%10)
-		n /= 10
+	if n < 10 { // si n est inférieur à 10, on retourne le caractère correspondant
+		return string('0' + n)
 	}
-
-	var res string
-
-	for i := len(digits) - 1; i >= 0; i-- {
-		res += string(digits[i] + 48)
-	}
-
-	return res
+	return IntToString(n/10) + string('0'+n%10) // sinon, on divise n par 10 et on ajoute le reste à la fin
 }
 
 func PrintNbr(n int) {
-	if n < 0 {
-		z01.PrintRune('-')
-		n *= -1
-	}
-	var digits []int
-
-	s := Itoa(n)
-
-	for _, m := range s {
-		digits = append(digits, int(m-'0'))
-	}
-
-	for _, val := range digits {
-		z01.PrintRune(rune(val + 48))
+	s := IntToString(n)   // convertit n en chaîne de caractères
+	for _, r := range s { // parcourt chaque caractère de s
+		z01.PrintRune(r) // affiche le caractère comme une rune
 	}
 }
