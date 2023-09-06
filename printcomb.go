00b3d908bd977ea92800c041459ed3e02409993b
Lucas <unmecpasfort@gmail.com>
2023-09-06 12:04:10 +0200

    PrintComb 3

--- printcomb.go
+++ printcomb.go
@@ -1,4 +1,4 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
@@ -7,12 +7,16 @@ func PrintComb() {
 		for j := '0'; j <= '9'; j++ {
 			for k := '0'; k <= '9'; k++ {
 				if i != j && i != k && j != k {
-					z01.PrintRune(rune(i))
-					z01.PrintRune(rune(j))
-					z01.PrintRune(rune(k))
-					if i != 9 && j != 8 && k != 7 {
-						z01.PrintRune(rune(','))
-						z01.PrintRune(rune(' '))
+					if i < j && i < k && j < k {
+						z01.PrintRune(rune(i))
+						z01.PrintRune(rune(j))
+						z01.PrintRune(rune(k))
+						if i != '7' || j != '8' || k != '9' {
+							z01.PrintRune(rune(','))
+							z01.PrintRune(rune(' '))
+						} else {
+							z01.PrintRune(rune('\n'))
+						}
 					}
 				}
 			}
