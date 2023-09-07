4e4599795e9ce9727cf2e727aff138e4b7ab1033
Lucas <unmecpasfort@gmail.com>
2023-09-07 16:31:33 +0200

    abort 3

--- abort.go
+++ abort.go
@@ -1,9 +1,17 @@
 package piscine
 
-import "sort"
-
 func Abort(a, b, c, d, e int) int {
 	args := []int{a, b, c, d, e}
-	sort.Sort(sort.IntSlice(args))
+	var len = 5
+	var temp int
+	for i := 0; i < len; i++ {
+		for j := 0; j < len; j++ {
+			if args[j] > args[j+1] {
+				temp = args[j]
+				args[j] = args[j+1]
+				args[j+1] = temp
+			}
+		}
+	}
 	return args[2]
 }
