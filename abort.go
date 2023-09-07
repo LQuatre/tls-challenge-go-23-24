e604f64d6f689316673682ba30f86ede25c53989
Lucas <unmecpasfort@gmail.com>
2023-09-07 16:34:51 +0200

    abort 4

--- abort.go
+++ abort.go
@@ -5,7 +5,7 @@ func Abort(a, b, c, d, e int) int {
 	var len = 5
 	var temp int
 	for i := 0; i < len; i++ {
-		for j := 0; j < len; j++ {
+		for j := 0; j < len-1; j++ {
 			if args[j] > args[j+1] {
 				temp = args[j]
 				args[j] = args[j+1]
