826e9113c37adc302015799d459e577c3af3ba03
Lucas <unmecpasfort@gmail.com>
2023-09-07 16:53:44 +0200

    enigma 3

--- enigma.go
+++ enigma.go
@@ -1,12 +1,12 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	e := ***a
-	f := *b
-	g := *******c
-	h := ****d
-	***a = g
-	*******c = e
-	****d = f
-	*b = h
+	vala := ***a
+	valb := *b
+	valc := *******c
+	vald := ****d
+	***a = valc
+	*******c = vald
+	****d = valb
+	*b = vala
 }
