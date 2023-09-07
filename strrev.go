d08638a17ee61a675657ea1d8a786ac7d0a86c70
Lucas <unmecpasfort@gmail.com>
2023-09-07 14:48:18 +0200

    StrLen 3

--- strrev.go
+++ strrev.go
@@ -2,9 +2,8 @@ package piscine
 
 func StrRev(s string) string {
 	result := ""
-	l := StrLen(s)
-	for i := l; i != 0; i-- {
-		result = i + result
+	for _, r := range s {
+		result = string(r) + result
 	}
 	return result
 }
