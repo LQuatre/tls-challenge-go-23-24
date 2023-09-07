22d3c33621116fa9dbbd4cb9c995df3ee57a2300
Lucas <unmecpasfort@gmail.com>
2023-09-07 14:25:59 +0200

    PrintStr 12

--- printnbr.go
+++ printnbr.go
@@ -4,10 +4,27 @@ import (
 	"github.com/01-edu/z01"
 )
 
+func UintToString(n uint) string {
+	var res string
+	for n > 0 {
+		res = string(n%10+48) + res
+		n /= 10
+	}
+	return res
+}
+
+func IntToString(n int) string {
+	if n < 0 {
+		z01.PrintRune('-')
+		return UintToString(uint(-n))
+	}
+	return UintToString(uint(n))
+}
+
 func PrintNbr(n int) {
 	if n == 0 {
 		z01.PrintRune('0')
 	}
-	s := string(n)
+	s := IntToString(n)
 	PrintStr(s)
 }
