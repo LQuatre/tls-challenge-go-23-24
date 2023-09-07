680fd0d487d3b92da1ef04705171027dbab08647
Lucas <unmecpasfort@gmail.com>
2023-09-07 16:49:17 +0200

    enigma

---
+++ enigma.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	e := ***a
+	f := *b
+	g := *******c
+	h := ****d
+
+	***a = g
+	*******c = e
+	****d = f
+	*b = h
+
+}
--- main/main.go
+++ main/main.go
@@ -6,6 +6,39 @@ import (
 )
 
 func main() {
-	middle := piscine.Abort(2, 3, 8, 5, 7)
-	fmt.Println(middle)
+	x := 5
+	y := &x
+	z := &y
+	a := &z
+
+	w := 2
+	b := &w
+
+	u := 7
+	e := &u
+	f := &e
+	g := &f
+	h := &g
+	i := &h
+	j := &i
+	c := &j
+
+	k := 6
+	l := &k
+	m := &l
+	n := &m
+	d := &n
+
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
+
+	piscine.Enigma(a, b, c, d)
+
+	fmt.Println("After using Enigma")
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
 }
