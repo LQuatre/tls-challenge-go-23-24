f81cf093ab40caffbbec224bb6caafb38966f028
Lucas <unmecpasfort@gmail.com>
2023-09-08 09:51:39 +0200

    BasicAtoi2

--- basicatoi2.go
+++ basicatoi2.go
@@ -6,7 +6,7 @@ func BasicAtoi2(s string) int {
 	num := 0
 	for i := 0; i < n; i++ {
 		if arrayStr[i] < '0' || arrayStr[i] > '9' {
-			return num
+			return 0
 		} else {
 			num *= 10
 			num += int(arrayStr[i]) - '0'
