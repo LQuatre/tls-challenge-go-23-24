666e4906090d33029111f82c73cbcec9db835c92
Lucas <unmecpasfort@gmail.com>
2023-09-08 14:44:14 +0200

    RecursiveFactorial 4

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,15 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb != 0 || nb < 20 {
-		if nb == 1 {
-			return 1
+	if nb < 25 {
+		if nb > 0 {
+			if nb == 1 {
+				return 1
+			} else {
+				return nb * RecursiveFactorial(nb-1)
+			}
 		} else {
-			return nb * RecursiveFactorial(nb-1)
+			return 0
 		}
 	} else {
 		return 0
