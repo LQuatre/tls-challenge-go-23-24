924a490ac7195281657543ad7fed485c27e6bdb4
Lucas <unmecpasfort@gmail.com>
2023-09-08 10:04:39 +0200

    Abandon Atoi lmao

--- atoi.go
+++ atoi.go
@@ -1,16 +1,63 @@
 package piscine
 
 func Atoi(s string) int {
-	arrayStr := []rune(s)
-	n := len(s)
-	num := 0
-	for i := 0; i < n; i++ {
-		if arrayStr[i] < '0' || arrayStr[i] > '9' {
-			return 0
-		} else {
-			num *= 10
-			num += int(arrayStr[i]) - '0'
+
+	return convert(clean(s))
+}
+
+func clean(s string) (sign int, abs []rune) {
+	a := []rune(s)
+
+	a = trimspace1(a)
+	if s == "" {
+		return
+	}
+
+	switch a[0] {
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		sign, abs = 1, a
+	case '+':
+		sign, abs = 1, a[1:]
+	case '-':
+		sign, abs = -1, a[1:]
+	default:
+		return
+	}
+
+	for i, b := range abs {
+		if b < '0' || '9' < b {
+			abs = abs[:i]
+			break
 		}
 	}
-	return num
+
+	return
+}
+
+func convert(sign int, absStr []rune) int {
+	abs := 0
+
+	for _, b := range absStr {
+		abs = abs*10 + int(b-'0')
+
+	}
+
+	return sign * abs
+}
+func trimspace1(a []rune) []rune {
+	for i := 0; i <= len(a)-1; i++ {
+		if a[i] == '-' && i != 0 || a[i] == '+' && i != 0 {
+			i = '0'
+
+			return []rune{'0'}
+		}
+		if a[i] == ' ' || a[i] == '.' || a[i] >= 'a' {
+
+			return []rune{'0'}
+
+		}
+	}
+
+	return a
+
 }
--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,17 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-	fmt.Println(piscine.Atoi("12345"))
-	fmt.Println(piscine.Atoi("0000000012345"))
-	fmt.Println(piscine.Atoi("012 345"))
-	fmt.Println(piscine.Atoi("Hello World!"))
-	fmt.Println(piscine.Atoi("+1234"))
-	fmt.Println(piscine.Atoi("-1234"))
-	fmt.Println(piscine.Atoi("++1234"))
-	fmt.Println(piscine.Atoi("--1234"))
+	for letter := 97; letter < 123; letter++ {
+		z01.PrintRune(rune(letter))
+	}
+	z01.PrintRune('\n')
 }
