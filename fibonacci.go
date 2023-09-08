568a0575dfce62841ba9b988f5e245c2ba70fba2
Lucas <unmecpasfort@gmail.com>
2023-09-08 15:25:42 +0200

    Fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Fibonacci(index int) int {
+	switch {
+	case index < 0:
+		return 0
+	case index == 0:
+		return 0
+	case index == 1:
+		return 1
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
+}
