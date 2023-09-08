bbab49b18927a14338a1065ff1a39c7063d21f63
Lucas <unmecpasfort@gmail.com>
2023-09-08 09:50:30 +0200

    BasicAtoi 2

---
+++ basicatoi2.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	arrayStr := []rune(s)
+	n := len(s)
+	num := 0
+	for i := 0; i < n; i++ {
+		if arrayStr[i] < '0' || arrayStr[i] > '9' {
+			return num
+		} else {
+			num *= 10
+			num += int(arrayStr[i]) - '0'
+		}
+	}
+	return num
+}
