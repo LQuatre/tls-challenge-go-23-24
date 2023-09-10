fe5c4e90aec039354ad8c8bff29a74995b24c35d
Lucas <unmecpasfort@gmail.com>
2023-09-10 20:20:57 +0200

    JumpOver 3

--- jumpover.go
+++ jumpover.go
@@ -1,18 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
-var first = false
-
 func JumpOver(str string) string {
-	if first == false {
-		first = true
-	} else {
-		z01.PrintRune(10)
-	}
 	var result string
 	for i := 2; i < StrLen(str); i += 3 {
 		result += string(str[i])
 	}
-	return result
+	return result + "\n"
 }
