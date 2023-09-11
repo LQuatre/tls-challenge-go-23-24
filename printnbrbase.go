8f914be9a220295afa2f92e9ea67bbbdb4821902
Lucas <unmecpasfort@gmail.com>
2023-09-11 12:13:52 +0200

    PrintNbrBase

--- printnbrbase.go
+++ printnbrbase.go
@@ -2,11 +2,25 @@ package piscine
 
 import "github.com/01-edu/z01"
 
+func CharIsNotUnique(str string) bool {
+	for i, r := range str {
+		for j := i + 1; j < len(str); j++ {
+			if r == rune(str[j]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func BaseIsValid(base string) bool {
-	if len(base) < 2 {
+	if base == "" {
 		return false
 	}
-	for i := 0; i < len(base)-1; i++ {
+	for i, r := range base {
+		if r == '-' || r == '+' {
+			return false
+		}
 		for j := i + 1; j < len(base); j++ {
 			if base[i] == base[j] {
 				return false
@@ -17,16 +31,23 @@ func BaseIsValid(base string) bool {
 }
 
 func PrintNbrBase(nbr int, base string) {
-	if BaseIsValid(base) {
+	if !BaseIsValid(base) {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
+	} else {
 		if nbr < 0 {
-			PrintStr("-")
+			z01.PrintRune('-')
 			nbr = -nbr
 		}
 		if nbr >= len(base) {
 			PrintNbrBase(nbr/len(base), base)
 		}
 		z01.PrintRune(rune(base[nbr%len(base)]))
-	} else {
-		PrintStr("NV")
+	}
+	if CharIsNotUnique(base) {
+		z01.PrintRune('N')
+		z01.PrintRune('V')
+		return
 	}
 }
