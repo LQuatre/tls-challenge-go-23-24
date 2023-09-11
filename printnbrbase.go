da5ad38c8b0a2b84f5eee5b0c920b471367e6e76
Lucas <unmecpasfort@gmail.com>
2023-09-11 13:26:39 +0200

    PrintNbrBase

--- printnbrbase.go
+++ printnbrbase.go
@@ -2,66 +2,53 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func checkBase(base string) bool {
-	if len(base) < 2 {
-		return false
+func Reverse(s string) string {
+	var reverse string
+	for i := len(s) - 1; i >= 0; i-- {
+		reverse += string(s[i])
 	}
-	for i := 0; i < len(base)-1; i++ {
-		for j := i + 1; j < len(base); j++ {
-			if base[i] == base[j] || base[i] == '+' || base[i] == '-' {
-				return false
-			}
-		}
-	}
-	return true
+	return reverse
 }
 
-func IsUnique(str string) bool {
-	for i := 0; i < len(str)-1; i++ {
-		for j := i + 1; j < len(str); j++ {
-			if str[i] == str[j] || str[i] == '+' || str[i] == '-' {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func ContainsNotValidChar(str string) bool {
+func Count(str, substr string) int {
+	count := 0
 	for i := 0; i < len(str); i++ {
-		if str[i] == '+' || str[i] == '-' {
-			return true
+		if str[i] == substr[0] {
+			if str[i:i+len(substr)] == substr {
+				count++
+			}
 		}
 	}
-	return false
+	return count
 }
 
-func PrintNbrBase(nbr int, base string) {
-	if !checkBase(base) {
-		z01.PrintRune('N')
-		z01.PrintRune('V')
-		return
+func PrintNbrBase(nbr int, str string) {
+	indx := 0
+	for _, res := range str {
+		if string(res) == "-" || string(res) == "+" || Count(str, string(res)) > 1 {
+			indx = 1
+			break
+		}
 	}
-	if !IsUnique(base) {
-		z01.PrintRune('N')
-		z01.PrintRune('V')
-		return
+	if indx == 1 || len(str) < 2 {
+		PrintStr("NV")
+	} else if 2147483647 < nbr || -2147483648 > nbr {
+		PrintStr("NV")
+	} else {
+		if nbr < 0 {
+			z01.PrintRune('-')
+			nbr *= -1
+		}
+		i := 0
+		nan := ""
+		for nbr >= len(str) {
+			if nbr >= len(str) {
+				nan += string(str[nbr%len(str)])
+				nbr = nbr / len(str)
+				i++
+			}
+		}
+		nan += string(str[nbr])
+		PrintStr(Reverse(nan))
 	}
-	if ContainsNotValidChar(base) {
-		z01.PrintRune('N')
-		z01.PrintRune('V')
-		return
-	}
-	if nbr < 0 {
-		z01.PrintRune('-')
-		nbr *= -1
-	}
-	if nbr == 0 {
-		z01.PrintRune(rune(base[0]))
-		return
-	}
-	if nbr >= len(base) {
-		PrintNbrBase(nbr/len(base), base)
-	}
-	z01.PrintRune(rune(base[nbr%len(base)]))
 }
