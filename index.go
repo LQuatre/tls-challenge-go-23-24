53039cc8ae85f99f8b41e6b1195a22b0e674bb81
Lucas <unmecpasfort@gmail.com>
2023-09-11 10:18:37 +0200

    Index marche mtn

--- index.go
+++ index.go
@@ -1,19 +1,17 @@
 package piscine
 
 func Index(s string, toFind string) int {
-	count := 0
-	count2 := 0
-	n := len(toFind)
-	for _, r := range s {
-		for _, v := range toFind {
-			if r == v {
-				count2++
+	lenS := len(s)
+	lenToFind := len(toFind)
+	if lenToFind == 0 {
+		return 0
+	}
+	for i := 0; i < lenS; i++ {
+		if s[i] == toFind[0] {
+			if toFind == s[i:i+lenToFind] {
+				return i
 			}
 		}
-		if n == count2 {
-			return count - n + 1
-		}
-		count++
 	}
 	return -1
 }
