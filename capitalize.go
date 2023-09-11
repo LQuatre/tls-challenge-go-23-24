b19d7b60b36294d94f85f97357fd24c522c8b66c
Lucas <unmecpasfort@gmail.com>
2023-09-11 11:41:54 +0200

    Capitalize 2

--- capitalize.go
+++ capitalize.go
@@ -1,12 +1,16 @@
 package piscine
 
+func IsLetterOrDigit(r rune) bool {
+	return (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')
+}
+
 func Capitalize(s string) string {
 	var result string
 	for i, v := range s {
 		if v >= 'a' && v <= 'z' {
 			if i == 0 {
 				result += string(v - 32)
-			} else if s[i-1] >= 'a' && s[i-1] <= 'z' || s[i-1] >= 'A' && s[i-1] <= 'Z' || s[i-1] >= '0' && s[i-1] <= '9' {
+			} else if IsLetterOrDigit(rune(s[i-1])) {
 				result += string(v)
 			} else {
 				result += string(v - 32)
@@ -14,7 +18,7 @@ func Capitalize(s string) string {
 		} else if v >= 'A' && v <= 'Z' {
 			if i == 0 {
 				result += string(v)
-			} else if s[i-1] >= 'a' && s[i-1] <= 'z' || s[i-1] >= 'A' && s[i-1] <= 'Z' || s[i-1] >= '0' && s[i-1] <= '9' {
+			} else if IsLetterOrDigit(rune(s[i-1])) {
 				result += string(v + 32)
 			} else {
 				result += string(v)
