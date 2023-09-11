b71020ef841a4c020aa66fdbfef2966f4868879d
Lucas <unmecpasfort@gmail.com>
2023-09-11 10:41:31 +0200

    IsAlpha

--- islower.go
+++ islower.go
@@ -1,11 +1,10 @@
 package piscine
 
 func IsLower(s string) bool {
-	lower := true
 	for _, r := range s {
 		if 'a' > r || r > 'z' {
-			lower = false
+			return false
 		}
 	}
-	return lower
+	return true
 }
--- isupper.go
+++ isupper.go
@@ -1,11 +1,10 @@
 package piscine
 
 func IsUpper(s string) bool {
-	upper := true
 	for _, r := range s {
 		if 'A' > r || r > 'Z' {
-			upper = false
+			return false
 		}
 	}
-	return upper
+	return true
 }
