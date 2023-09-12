87095ffa7330c8953fa42c37bb3423b4b14fa2c2
Lucas <unmecpasfort@gmail.com>
2023-09-12 16:15:59 +0200

    rockandroll 3 PUTAIN DE \n

--- rockandroll.go
+++ rockandroll.go
@@ -2,18 +2,18 @@ package piscine
 
 func RockAndRoll(n int) string {
 	if n < 0 {
-		return "error: number is negative"
+		return "error: number is negative\n"
 	}
 	if n == 0 {
-		return "error: non divisible"
+		return "error: non divisible\n"
 	}
 	if n%2 == 0 && n%3 == 0 {
-		return "rock and roll"
+		return "rock and roll\n"
 	} else if n%2 == 0 {
-		return "rock"
+		return "rock\n"
 	} else if n%3 == 0 {
-		return "roll"
+		return "roll\n"
 	} else {
-		return "error: non divisible"
+		return "error: non divisible\n"
 	}
 }
