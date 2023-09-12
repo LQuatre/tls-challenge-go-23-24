b4ec2e38fd7e108efaab6ac2b34d199c21e93d65
Lucas <unmecpasfort@gmail.com>
2023-09-12 12:54:07 +0200

    NbrConvertAlpha 2

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"os"
-
-	"github.com/01-edu/z01"
 )
 
 func BasicAtoi2(s string) int {
@@ -27,23 +25,22 @@ func main() {
 	for i := 1; i < len(Args); i++ {
 		table = append(table, BasicAtoi2(Args[i]))
 	}
+	// flags "--upper"
 	upper := false
-	if len(Args) > 1 {
-		if Args[1] == "--upper" {
-			upper = true
-		}
+	if Args[1] == "--upper" {
+		upper = true
 	}
 	if upper == false {
 		for i := 0; i < len(table); i++ {
 			table[i] = table[i] + 96
 		}
 	} else {
-		for i := 1; i < len(table); i++ {
+		for i := 0; i < len(table); i++ {
 			table[i] = table[i] + 64
 		}
 	}
 	for i := 0; i < len(table); i++ {
-		z01.PrintRune(rune(table[i]))
+		print(string(table[i]))
 	}
-	z01.PrintRune('\n')
+	println()
 }
