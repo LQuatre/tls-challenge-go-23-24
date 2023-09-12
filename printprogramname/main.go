2385729b4b0731cc8187ae894933ad798966cfcf
Lucas <unmecpasfort@gmail.com>
2023-09-12 10:29:23 +0200

    Print Program Name 20

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,11 +6,32 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main() {
-	Args := os.Args[0]
-	for k, v := range Args {
-		if k > 1 {
-			z01.PrintRune(rune(v))
+func DernierSlashEtLaSuite(s string) string {
+	var index int
+	for i, r := range s {
+		if r == '\\' {
+			index = i
 		}
 	}
+	return s[index:]
+}
+
+func RemoveExe(s string) string {
+	var index int
+	for i, r := range s {
+		if r == '.' {
+			index = i
+		}
+	}
+	return s[:index]
+}
+
+func main() {
+	Args := os.Args[0]
+	Args = DernierSlashEtLaSuite(Args)
+	Args = RemoveExe(Args)
+	for _, r := range Args {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
