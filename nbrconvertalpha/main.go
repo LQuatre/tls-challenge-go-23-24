8880f8a28e5c3ba4450ef745a16b45b501141fa1
Lucas <unmecpasfort@gmail.com>
2023-09-12 13:14:20 +0200

    NbrConvertAlpah

--- nbrconvertalpha/main.go
+++ nbrconvertalpha/main.go
@@ -7,52 +7,27 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 1 {
-		arguments := os.Args[1:]
-		arrayNumbers := []string{}
-		arrayLetters := []rune{}
-		for i := 1; i < 27; i++ {
-			if i < 10 {
-				arrayNumbers = append(arrayNumbers, string(rune(i+'0')))
-			}
-			if i >= 10 {
-				mod := i % 10
-				decimal := i / 10
-				arrayNumbers = append(arrayNumbers, (string(rune(decimal+'0')) + string(rune(mod+'0'))))
-			}
-		}
-		if arguments[0] != "--upper" {
-			for i := 'a'; i <= 'z'; i++ {
-				arrayLetters = append(arrayLetters, i)
-			}
-		}
-		if arguments[0] == "--upper" {
-			for i := 'A'; i <= 'Z'; i++ {
-				arrayLetters = append(arrayLetters, i)
-			}
-		}
-		for i := 0; i < len(arguments); i++ {
-			if arguments[i] != "--upper" {
-				if inArray(arguments[i], arrayNumbers) {
-					for index, value := range arrayNumbers {
-						if arguments[i] == value {
-							z01.PrintRune(arrayLetters[index])
-						}
-					}
-				} else {
-					z01.PrintRune(' ')
-				}
-			}
-		}
-		z01.PrintRune('\n')
-	}
-}
-
-func inArray(a string, b []string) bool {
-	for _, value := range b {
-		if a == value {
-			return true
+	Args := os.Args[1:]
+	upper := false
+	for _, v := range Args {
+		if v == "--upper" {
+			upper = true
 		}
 	}
-	return false
+	for _, arg := range Args {
+		numv := 0
+		for _, v := range arg {
+			numv = numv*10 + int(v-'0')
+		}
+		if numv >= 1 && numv <= 26 {
+			if upper == false {
+				z01.PrintRune(rune(numv + 96))
+			} else {
+				z01.PrintRune(rune(numv + 64))
+			}
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
 }
