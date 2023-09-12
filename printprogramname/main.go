4343c4bc93e7cb85091912cb1b301d0e887f14b4
Lucas <unmecpasfort@gmail.com>
2023-09-12 10:31:37 +0200

    Print Program Name 40

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,32 +6,12 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func DernierSlashEtLaSuite(s string) string {
-	var index int
-	for i, r := range s {
-		if r == '\\' {
-			index = i
-		}
-	}
-	return s[index:]
-}
-
-func RemoveExe(s string) string {
-	var index int
-	for i, r := range s {
-		if r == '.' {
-			index = i
-		}
-	}
-	return s[:index]
-}
-
 func main() {
 	Args := os.Args[0]
-	Args = DernierSlashEtLaSuite(Args)
-	Args = RemoveExe(Args)
-	for _, r := range Args {
-		z01.PrintRune(r)
+	for i, v := range Args {
+		if i > 1 {
+			z01.PrintRune(rune(v))
+		}
 	}
 	z01.PrintRune('\n')
 }
