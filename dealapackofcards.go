501f30cc49acbe6b7f01535a5bcd17765d357326
Lucas <unmecpasfort@gmail.com>
2023-09-13 12:34:21 +0200

    DealAPackOfCards

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,7 +3,7 @@ package piscine
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
 		PrintStr("Player: ")
-		for j := 0; j < len(deck); j++ {
+		for j := 0; j < 13; j++ {
 			PrintStr(string(deck[j]))
 			PrintStr(" ")
 		}
