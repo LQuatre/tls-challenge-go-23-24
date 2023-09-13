232d529f9955b1bc43de20e71d4f88b60ee582a5
Lucas <unmecpasfort@gmail.com>
2023-09-13 10:12:48 +0200

    Split j'avais pas compris lmao

--- split.go
+++ split.go
@@ -1,27 +1,17 @@
 package piscine
 
-func AddTemp2(result *string, temp *string) {
-	if len(*temp) > 0 {
-		if len(*result) > 0 {
-			*result += " "
-		}
-		*result += *temp
-		*temp = ""
-	}
-}
-
-func Split(s, sep string) string {
-	var result string
+func Split(s, sep string) []string {
+	var result []string
 	var temp string
 	for i := 0; i < len(s); i++ {
 		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
-			AddTemp2(&result, &temp)
+			AddTemp(&result, &temp)
 			i += len(sep) - 1
 			continue
 		}
 		temp += string(s[i])
 		if i == len(s)-1 {
-			AddTemp2(&result, &temp)
+			AddTemp(&result, &temp)
 		}
 	}
 	return result
