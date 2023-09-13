09bef587c96b06c121e607ddb053aea62c98b87e
Lucas <unmecpasfort@gmail.com>
2023-09-13 12:19:57 +0200

    main

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,13 +1,11 @@
 package piscine
 
-import "fmt"
-
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
-		fmt.Printf("Player %d : ", i+1)
+		PrintStr("Player" + Itoa(i+1) + ": ")
 		for j := i * 3; j < (i+1)*3; j++ {
-			fmt.Printf("%d ", deck[j])
+			PrintStr(Itoa(deck[j]) + " ")
 		}
-		fmt.Println()
+		PrintStr("\n")
 	}
 }
---
+++ itoa.go
@@ -0,0 +1,15 @@
+package piscine
+
+func Itoa(n int) string {
+	var result string
+	var temp string
+	if n == 0 {
+		return "0"
+	}
+	for n != 0 {
+		temp = string(n%10 + 48)
+		result = temp + result
+		n /= 10
+	}
+	return result
+}
