41b0a12b357038de44b7b6bf64927c21267f08d8
Lucas <unmecpasfort@gmail.com>
2023-09-13 12:35:21 +0200

    DealAPackOfCards

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,10 +1,12 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
 		PrintStr("Player: ")
 		for j := 0; j < 13; j++ {
-			PrintStr(string(deck[j]))
+			z01.PrintRune(deck[j]+4)
 			PrintStr(" ")
 		}
 		PrintStr("\n")
