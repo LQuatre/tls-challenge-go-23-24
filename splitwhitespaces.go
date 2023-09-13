cbf7b63239f859ba14fde665bc88cc7c5d111992
Lucas <unmecpasfort@gmail.com>
2023-09-13 09:51:13 +0200

    SplitWhiteSpace 43

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,18 +1,23 @@
 package piscine
 
+func AddTemp(result *[]string, temp *string) {
+	if len(*temp) > 0 {
+		*result = append(*result, *temp)
+		*temp = ""
+	}
+}
+
 func SplitWhiteSpaces(s string) []string {
 	var result []string
 	var temp string
 	for i := 0; i < len(s); i++ {
 		if s[i] == ' ' || s[i] == '\n' || s[i] == '\t' {
-			result = append(result, temp)
-			temp = ""
+			AddTemp(&result, &temp)
 			continue
 		}
 		temp += string(s[i])
 		if i == len(s)-1 {
-			result = append(result, temp)
-			temp = ""
+			AddTemp(&result, &temp)
 		}
 	}
 	return result
