ea3454a29237a101fd52677d8ba98bddd46dd5fc
Lucas <unmecpasfort@gmail.com>
2023-09-13 09:14:35 +0200

    AppendRange

--- main/main.go
+++ main/main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Print(piscine.LoafOfBread("deliciousbread"))
-	fmt.Print(piscine.LoafOfBread("This is a loaf of bread"))
-	fmt.Print(piscine.LoafOfBread("loaf"))
-	fmt.Print(piscine.LoafOfBread("Loaf of bread"))
-	fmt.Print(piscine.LoafOfBread("amazing loaf of bread"))
-	fmt.Print(piscine.LoafOfBread("This is a loaf of brea"))
+	fmt.Println(piscine.AppendRange(5, 10))
+	fmt.Println(piscine.AppendRange(10, 5))
 }
