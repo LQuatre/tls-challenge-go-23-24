6127915871d33b08e48a7cb32134c89daa01ede5
L4 <83190245+LQuatre@users.noreply.github.com>
2023-09-14 18:39:56 +0200

    test

--- ztail/main.go
+++ ztail/main.go
@@ -6,66 +6,67 @@ import (
 )
 
 func main() {
-	status := true
-	if len(os.Args) >= 4 {
-		numChars := os.Args[2]
-		files := os.Args[3:]
+	numChars := os.Args[2]
+	files := os.Args[3:]
 
-		for i := 0; i < len(files); i++ {
-			if err := tailFile(files[i], numChars); err != nil {
-				fmt.Printf("open %v: no such file or directory\n", files[i])
-				status = false
-				continue
-			}
+	for i := 0; i < len(files); i++ {
+		if err := tailFile(files[i], numChars); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
 		}
-	} else {
-		status = false
 	}
-	if !status {
-		os.Exit(1)
-	}
+	os.Exit(1)
 }
 
 func tailFile(filename string, numChars string) error {
-	option := []rune(os.Args[2])
-	value := IntConv(option)
 	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileInfo.Size()
+
+	numBytesToRead, err := parseNumChars(numChars)
+	if err != nil {
+		return err
+	}
+
+	if fileSize < numBytesToRead {
+		numBytesToRead = fileSize
+	}
+
+	_, err = file.Seek(fileSize-numBytesToRead, 0)
+	if err != nil {
+		return err
+	}
+
 	fmt.Println()
 	fmt.Printf("==> %s <==\n", filename)
 
-	file_stat, err := file.Stat()
+	buf := make([]byte, numBytesToRead)
+	_, err = file.Read(buf)
 	if err != nil {
-		fmt.Printf("could not obtain stat for file %v\n", filename)
+		return err
 	}
-	content := make([]byte, file_stat.Size())
-	file.Read(content)
-	contentinrune := []rune(string(content))
-	if len(contentinrune) >= value {
-		last_chars := make([]rune, value)
-		for j := 0; j < len(last_chars); j++ {
-			last_chars[j] = contentinrune[len(contentinrune)-value+j]
-		}
-		fmt.Print(string(last_chars))
-	} else {
-		fmt.Print(string(contentinrune))
-	}
+
+	fmt.Printf("%s", buf)
 	return nil
 }
 
-func IntConv(option []rune) int {
-	value := 0
-	for i := 0; i < len(option); i++ {
-		digit := int(option[i] - '0')
-		decimal := 1
-		for j := 0; j < len(option)-1-i; j++ {
-			decimal *= 10
+func parseNumChars(numChars string) (int64, error) {
+	var num int64
+	for _, char := range numChars {
+		if char >= '0' && char <= '9' {
+			num = num*10 + int64(char-'0')
+		} else {
+			return 0, fmt.Errorf("invalid number of characters: %s", numChars)
 		}
-		value += digit * decimal
 	}
-	return value
+	return num, nil
 }
