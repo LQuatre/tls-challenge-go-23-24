4b90ce101b0b85cdb3514dd0d50ebca062628e8d
Lucas <unmecpasfort@gmail.com>
2023-09-14 11:52:29 +0200

    cat

---
+++ cat/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func PrintStr(str string) {
+	for _, c := range str {
+		z01.PrintRune(c)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		PrintStr("File name missing\n")
+		return
+	}
+	for _, arg := range args {
+		bytes, err := ioutil.ReadFile(arg)
+		if err != nil {
+			PrintStr("ERROR: open " + arg + ": " + err.Error() + "\n")
+			continue
+		}
+		PrintStr(string(bytes))
+	}
+}
