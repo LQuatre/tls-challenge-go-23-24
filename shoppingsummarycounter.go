a159df6d3073e83816351aaf20ca3b6803ff24a7
Lucas <unmecpasfort@gmail.com>
2023-09-14 09:13:32 +0200

    ShoppingSummaryCounter

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,10 +1,15 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	result := make(map[string]int)
-	a := SplitWhiteSpaces2(str)
-	for _, v := range a {
-		result[v]++
+	wordList := SplitWhiteSpaces(str)
+	counts := make(map[string]int)
+	for _, word := range wordList {
+		_, ok := counts[word]
+		if ok {
+			counts[word] += 1
+		} else {
+			counts[word] = 1
+		}
 	}
-	return result
+	return counts
 }
