e23440fe29dd97f61127d6bcd2ad24c3d717f642
Lucas <unmecpasfort@gmail.com>
2023-09-14 10:25:24 +0200

    points 4

--- point/main.go
+++ point/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/01-edu/z01"
+
 type point struct {
 	x int
 	y int
@@ -10,11 +12,19 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func PrintStr(str string) {
+	for _, r := range str {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
 func main() {
 	points := &point{}
 	setPoint(points)
 	PrintStr("x = ")
-	PrintNbr(points.x)
+	z01.PrintRune(rune(points.x + 48))
 	PrintStr(", y = ")
-	PrintNbr(points.y)
+	z01.PrintRune(rune(points.y + 48))
+	PrintStr("")
 }
