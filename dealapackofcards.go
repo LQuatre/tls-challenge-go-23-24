b9385f14afc3d14e3079f5dbc28f8be0b3b14804
DIOT Lucas <dilucas@noreply.ytrack.learn.ynov.com>
2023-09-14 06:47:13 +0000

    ok

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func DealAPackOfCards(deck []int) {
+func DealAPackOfCards (deck []int) {
 	for i := 0; i < 4; i++ {
 		PrintStr("Player: ")
 		for j := 0; j < 13; j++ {
