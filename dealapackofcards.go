69cb2961921dd5dc702f6cda85bf90a1d337366f
Lucas <unmecpasfort@gmail.com>
2023-09-14 09:23:13 +0200

    Fix 3

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,7 +3,7 @@ package piscine
 func DealAPackOfCards(deck []int) {
 	result := ""
 	for i := 0; i < 4; i++ {
-		result += "Player: "
+		result += "Player "
 		result += IntToString(i + 1)
 		result += ":"
 		for j := i * 3; j < i*3+3; j++ {
