5465e14ab5dc321b21eac0040cae02459cd085dd
DIOT Lucas <unmecpasfort@gmail.com>
2023-09-18 09:53:54 +0200

    CountIf

--- main/main.go
+++ main/main.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	a := []int{1, 2, 3, 4, 5, 6}
-	result := piscine.Map(piscine.IsPrime, a)
-	fmt.Println(result)
+	tab1 := []string{"Hello", "how", "are", "you"}
+	tab2 := []string{"This", "1", "is", "4", "you"}
+	answer1 := piscine.CountIf(piscine.IsNumeric, tab1)
+	answer2 := piscine.CountIf(piscine.IsNumeric, tab2)
+	fmt.Println(answer1)
+	fmt.Println(answer2)
 }
