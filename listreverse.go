4f20b7db313b09fda4d7f171af523c7aff386594
DIOT Lucas <unmecpasfort@gmail.com>
2023-09-20 10:41:44 +0200

    J'avoue que mon précédent truc était bien guez

--- listreverse.go
+++ listreverse.go
@@ -4,14 +4,11 @@ func ListReverse(l *List) {
 	if l.Head == nil {
 		return
 	}
-	current := l.Head
-	var prev *NodeL
-	l.Tail = l.Head
-	for current != nil {
-		next := current.Next
-		current.Next = prev
-		prev = current
-		current = next
+	list := &List{}
+	it := l.Head
+	for it != nil {
+		ListPushFront(list, it.Data)
+		it = it.Next
 	}
-	l.Head = prev
+	l.Head = list.Head
 }
