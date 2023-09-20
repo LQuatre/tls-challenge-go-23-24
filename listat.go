c05fbea8d0795cc177e2c3bf063a4e6220980641
DIOT Lucas <unmecpasfort@gmail.com>
2023-09-20 09:53:08 +0200

    add content to listat.go

---
+++ listat.go
@@ -0,0 +1,14 @@
+package piscine
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	if pos < 0 {
+		return nil
+	}
+	for i := 0; i < pos; i++ {
+		if l == nil {
+			return nil
+		}
+		l = l.Next
+	}
+	return l
+}
