6f755094392dba5e8889fc8b21d712cdbfe1245d
DIOT Lucas <unmecpasfort@gmail.com>
2023-09-22 14:15:58 +0200

    modified:   btreeapplybylevel.go

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,10 +1,23 @@
 package piscine
 
+func BTreeApplyLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	if level == 1 {
+		f(root.Data)
+	} else if level > 1 {
+		BTreeApplyLevel(root.Left, level-1, f)
+		BTreeApplyLevel(root.Right, level-1, f)
+	}
+}
+
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyByLevel(root.Left, f)
-	BTreeApplyByLevel(root.Right, f)
+	level := BTreeLevelCount(root)
+	for i := 1; i <= level; i++ {
+		BTreeApplyLevel(root, i, f)
+	}
 }
