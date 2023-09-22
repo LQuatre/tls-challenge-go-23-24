fd90fbe1636ae8a733d4b9ba138e3debff85c815
DIOT Lucas <unmecpasfort@gmail.com>
2023-09-22 11:27:16 +0200

    modified btreeapplyinorder.go

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -1,7 +1,7 @@
-package piscine 
+package piscine
 
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
-	if root === nil {
+	if root == nil {
 		return
 	}
 	BTreeApplyInorder(root.Left, f)
